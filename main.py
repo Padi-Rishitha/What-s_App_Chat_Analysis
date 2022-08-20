c1d1791198b08c4920258aa6994c5d7202064ffe
padi-rishitha-08 <rishithapadi08@gmail.com>
2022-08-20 16:54:12 +0530

    start

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-# from flask import Flask
-# app = Flask(__name__)
-# @app.route("/")
-# def hello():
-#     return "Hello World!"
+
 import streamlit as st
-import preprocessor
+import preprocessor, helper
 st.sidebar.title("Whats app Chat Analyser")
 
 uploaded_file = st.sidebar.file_uploader("Choose a file")
@@ -19,5 +15,26 @@ if uploaded_file is not None:
      user_list = df['user'].unique().tolist()
      user_list.remove('group_notification')
      user_list.sort()
-     user_list.insert('Overall')
-     st.sidebar.selectbox('Show analysis wrt',user_list)
+     user_list.insert(0,'Overall')
+     selected_user = st.sidebar.selectbox('Show analysis wrt',user_list)
+
+     if st.sidebar.button("Show Analysis"):
+
+        num_messages, words, num_media_messages, num_links = helper.fetch_stats(selected_user,df)
+        col1, col2,col3,col4 =st.columns(4)
+        with col1:
+            st.subheader("Total Messages")
+            st.title(num_messages)
+
+        with col2:
+            st.subheader("Total words")
+            st.title(words)
+        with col3:
+            st.subheader("Media Shared")
+            st.title(num_media_messages)
+        with col4:
+            st.subheader("Links Shsred")
+            st.title(num_links)
+       #finding the busy person
+        if selected_user == 'Overall':
+            col1,col2 == st.columns(2)
